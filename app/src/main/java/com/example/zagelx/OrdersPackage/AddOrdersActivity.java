cc407fb2af9a145e94bb99a3de36fd5977dd3332
anas <anas1243@gmail.com>
2019-04-28 13:44:38 +0200

    feat: display the right vehicle image + modify the onclick listeners of all buttons
    i modified the logic of coloring the selected vehicle via drwable selectors for each vehicle but, it didn't give the intended result so i made it programatically. also, i modified the logic of listening on all buttons in AddOrdersActivity

--- app/src/main/java/com/example/zagelx/OrdersPackage/AddOrdersActivity.java
+++ app/src/main/java/com/example/zagelx/OrdersPackage/AddOrdersActivity.java
@@ -7,6 +7,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
 import android.util.Log;
@@ -22,6 +24,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.zagelx.Authentication.MainActivity;
 import com.example.zagelx.Models.BirthDate;
 import com.example.zagelx.Models.Orders;
 import com.example.zagelx.Models.Users;
@@ -46,10 +49,11 @@ import com.google.firebase.storage.UploadTask;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-public class AddOrdersActivity extends AppCompatActivity {
+public class AddOrdersActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AddOrdersActivity";
     private static final int RC_PHOTO_PICKER = 2;
+    private final int sdk = android.os.Build.VERSION.SDK_INT;
 
     private ImageView packageImage;
     private ImageView editPackageImage;
@@ -83,7 +87,7 @@ public class AddOrdersActivity extends AppCompatActivity {
     private UploadTask uploadTask;
     private Uri selectedImageUri;
 
-    private String  merchantId, merchantImageURL, getMerchantName, oImageUrl, oName, dPrice, RMobile, oPrice, oVehicle, oDescription;
+    private String merchantId, merchantImageURL, getMerchantName, oImageUrl, oName, dPrice, RMobile, oPrice, oVehicle, oDescription;
     private boolean isPrePaid = false;
     private BirthDate dDate;
 
@@ -123,12 +127,11 @@ public class AddOrdersActivity extends AppCompatActivity {
         destinationET = findViewById(R.id.destination_txt_view);
 
 
-        AddOrderButton = findViewById(R.id.button_register);
+        AddOrderButton = findViewById(R.id.button_yalla);
 
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mOrdersDatabaseReference = mFirebaseDatabase.getReference().child("Orders");
-
 
 
         mFirebaseStorage = FirebaseStorage.getInstance();
@@ -153,40 +156,21 @@ public class AddOrdersActivity extends AppCompatActivity {
         });
 
 
+        icCar.setOnClickListener(this);
+        icBus.setOnClickListener(this);
+        icTrain.setOnClickListener(this);
+        icMetro.setOnClickListener(this);
+        icMotorcycle.setOnClickListener(this);
+        icNosNal.setOnClickListener(this);
+        packageImage.setOnClickListener(this);
+        editPackageImage.setOnClickListener(this);
 
-        packageImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/jpeg");
-                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
-            }
-        });
-        editPackageImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/jpeg");
-                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
-                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
-            }
-        });
+        AddOrderButton.setOnClickListener(this);
 
-        AddOrderButton.setOnClickListener(new View.OnClickListener() {
+        isPrePaidSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                validateTheUser();
-
-            }
-        });
-
-        isPrePaidSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-        {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-            {
-                if(isChecked)
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
                     isPrePaid = true;
                 else
                     isPrePaid = false;
@@ -216,7 +200,7 @@ public class AddOrdersActivity extends AppCompatActivity {
         oVehicle = vehicle.getText().toString().trim();
         oDescription = packageDescriptionET.getText().toString().trim();
         dDate = new BirthDate(deliveryDateDP.getYear(),
-                deliveryDateDP.getMonth()+1,deliveryDateDP.getDayOfMonth());
+                deliveryDateDP.getMonth() + 1, deliveryDateDP.getDayOfMonth());
 //        if (uName.equals("")) {
 //            //Snackbar.make(findViewById(R.id.scroll_view), "Enter your name", Snackbar.LENGTH_LONG).show();
 //            userName.setError("Enter your name");
@@ -247,15 +231,14 @@ public class AddOrdersActivity extends AppCompatActivity {
                     mPackagePhotoStorageReference.child(selectedImageUri.getLastPathSegment());
             try {
                 bmp = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
-            }catch (IOException e){
-                Log.e(TAG, "uploadOrderImageAndProceed: ", e.fillInStackTrace() );
+            } catch (IOException e) {
+                Log.e(TAG, "uploadOrderImageAndProceed: ", e.fillInStackTrace());
             }
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             bmp.compress(Bitmap.CompressFormat.JPEG, 25, baos);
             byte[] data = baos.toByteArray();
             //uploading the image
             uploadTask = photoRef.putBytes(data);
-
 
 
             uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -303,20 +286,117 @@ public class AddOrdersActivity extends AppCompatActivity {
                             Orders order = new Orders(merchantId, merchantImageURL, getMerchantName, oName, oImageUrl
                                     , oDescription, oPrice
                                     , isPrePaid, dDate,
-                                    dPrice, oVehicle,"alexandria","cairo",RMobile);
+                                    dPrice, oVehicle, "alexandria", "cairo", RMobile);
                             mOrdersDatabaseReference.push().setValue(order);
 
 
                             Toast.makeText(AddOrdersActivity.this, "your order has been add!", Toast.LENGTH_SHORT).show();
                             Intent i = new Intent(AddOrdersActivity.this, OrdersActivity.class);
                             startActivity(i);
-                        }else{
+                        } else {
                             Toast.makeText(AddOrdersActivity.this, "cant upload package image please try again!", Toast.LENGTH_SHORT).show();
 
                         }
                     }
                 }
             });
+
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ic_bus:
+                clearVehicleOptionsColor();
+                vehicle.setText("Bus");
+                if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    icBus.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_bus_yellow));
+                } else {
+                    icBus.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_bus_yellow));
+                }
+                break;
+            case R.id.ic_car:
+                clearVehicleOptionsColor();
+                vehicle.setText("Car");
+                if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    icCar.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_car_yellow));
+                } else {
+                    icCar.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_car_yellow));
+                }
+                break;
+            case R.id.ic_train:
+                clearVehicleOptionsColor();
+                vehicle.setText("Train");
+                if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    icTrain.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_train_yellow));
+                } else {
+                    icTrain.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_train_yellow));
+                }
+                break;
+            case R.id.ic_motorcycle:
+                clearVehicleOptionsColor();
+                vehicle.setText("MotorCycle");
+                if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    icMotorcycle.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_motorcycle_yellow));
+                } else {
+                    icMotorcycle.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_motorcycle_yellow));
+                }
+                break;
+            case R.id.ic_metro:
+                clearVehicleOptionsColor();
+                vehicle.setText("Metro");
+                if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    icMetro.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_metro_yellow));
+                } else {
+                    icMetro.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_metro_yellow));
+                }
+                break;
+            case R.id.ic_nos_na2l:
+                clearVehicleOptionsColor();
+                vehicle.setText("Nos Na2l");
+                if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    icNosNal.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_nos_na2l_yellow));
+                } else {
+                    icNosNal.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_nos_na2l_yellow));
+                }
+                break;
+            case R.id.edit_package_image:
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, "Complete action using"), RC_PHOTO_PICKER);
+
+                break;
+            case R.id.package_image:
+                Intent intent1 = new Intent(Intent.ACTION_GET_CONTENT);
+                intent1.setType("image/jpeg");
+                intent1.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent1, "Complete action using"), RC_PHOTO_PICKER);
+
+                break;
+            case R.id.button_yalla:
+                validateTheUser();
+                break;
+        }
+    }
+
+    public void clearVehicleOptionsColor() {
+        if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            icBus.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_bus));
+            icCar.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_car));
+            icTrain.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_train));
+            icMetro.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_metro));
+            icMotorcycle.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_motorcycle));
+            icNosNal.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.vehicle_nos_na2l));
+
+        } else {
+            icBus.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_bus));
+            icCar.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_car));
+            icTrain.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_train));
+            icMetro.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_metro));
+            icMotorcycle.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_motorcycle));
+            icNosNal.setBackground(ContextCompat.getDrawable(this, R.drawable.vehicle_nos_na2l));
 
         }
     }
