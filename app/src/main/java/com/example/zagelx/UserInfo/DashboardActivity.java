39b7f64c6f0f1cdfb590c953411f2241551e8897
anas <anas1243@gmail.com>
2019-06-24 19:46:03 +0200

    feat: Add dashBoard contains users'-only orders

--- app/src/main/java/com/example/zagelx/UserInfo/DashboardActivity.java
+++ app/src/main/java/com/example/zagelx/UserInfo/DashboardActivity.java
@@ -1,15 +1,129 @@
 package com.example.zagelx.UserInfo;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.ListView;
 
+import com.example.zagelx.Models.Orders;
+import com.example.zagelx.Models.Users;
+import com.example.zagelx.OrdersPackage.OrdersActivity;
+import com.example.zagelx.OrdersPackage.OrdersAdapter;
 import com.example.zagelx.R;
+import com.example.zagelx.Utilities.DrawerUtil;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DashboardActivity extends AppCompatActivity {
+    private ListView mOrdersListView;
+    private DashboardAdapter mOrdersAdapter;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mOrdersDatabaseReference;
+    private DatabaseReference mUserDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    private ValueEventListener mUserEventListener;
+
+    private FirebaseUser user;
+    private Users currentUser;
+
+    private Query query;
+
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    DrawerUtil drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mOrdersDatabaseReference = mFirebaseDatabase.getReference().child("Orders");
+
+        mUserDatabaseReference = mFirebaseDatabase.getReference().child("Users");
+
+
+        mOrdersListView = findViewById(R.id.main_list);
+
+
+        final List<Orders> ordersList = new ArrayList<>();
+        mOrdersAdapter = new DashboardAdapter(DashboardActivity.this, R.layout.order_list_dashboard, ordersList);
+        mOrdersListView.setAdapter(mOrdersAdapter);
+
+        Snackbar snackbar = Snackbar
+                .make(findViewById(R.id.main_main_layout), "الشحنات الخاصة بك !", Snackbar.LENGTH_LONG);
+        snackbar.show();
+
+        if (user != null) {
+
+            mUserEventListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    currentUser = dataSnapshot.getValue(Users.class);
+
+                    ButterKnife.bind(DashboardActivity.this);
+                    setSupportActionBar(toolbar);
+
+                    drawer = new DrawerUtil(currentUser.getName()
+                            , currentUser.getMobileNumber(),currentUser.getProfilePictureURL());
+                    drawer.getDrawer(DashboardActivity.this, toolbar);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+
+            mUserDatabaseReference.child(user.getUid())
+                    .addListenerForSingleValueEvent(mUserEventListener);
+
+
+            query = mOrdersDatabaseReference.orderByChild("merchantId").equalTo(user.getUid());
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        for (DataSnapshot order : dataSnapshot.getChildren()) {
+                            Orders currentOrder = order.getValue(Orders.class);
+                            mOrdersAdapter.add(currentOrder);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+
+        } else {
+            AuthUI.getInstance().signOut(this);
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/zagelx/UserInfo/DashboardAdapter.java
@@ -0,0 +1,111 @@
+package com.example.zagelx.UserInfo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.zagelx.Models.BirthDate;
+import com.example.zagelx.Models.Orders;
+import com.example.zagelx.OrdersPackage.OrderDetails;
+import com.example.zagelx.R;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class DashboardAdapter extends ArrayAdapter<Orders> {
+    Context context ;
+    public DashboardAdapter(Context context, int resource, List<Orders> objects) {
+        super(context, resource, objects);
+        this.context = context;
+    }
+
+
+    @Override
+    public Orders getItem(int position) {
+        return super.getItem(super.getCount() - position - 1);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.order_list_dashboard
+                    , parent, false);
+        }
+
+        View listItemView = convertView;
+
+
+        CircleImageView packageImageIV = convertView.findViewById(R.id.package_image);
+        TextView packageNameTV = convertView.findViewById(R.id.package_name);
+        TextView deliveryDateTV = convertView.findViewById(R.id.delivery_date);
+        TextView deliveryPriceTV = convertView.findViewById(R.id.delivery_price);
+
+        TextView sourceTV = convertView.findViewById(R.id.source_txt_view);
+        TextView destinationTV = convertView.findViewById(R.id.destination_txt_view);
+        ImageView vehicleImageIV = convertView.findViewById(R.id.vehicle_image);
+
+
+        final Orders CurrentOrder = getItem(position);
+        BirthDate oDate = CurrentOrder.getDeliveryDate();
+
+
+
+
+        Glide.with(packageImageIV.getContext())
+                .load(CurrentOrder.getPackageImageURL())
+                .into(packageImageIV);
+
+        Log.e("OrdersAdapter", "getView: " + CurrentOrder.getPackageImageURL());
+        packageNameTV.setText(CurrentOrder.getPackageName());
+        final String orderDate = oDate.getYear() + "-" + oDate.getMonth() + "-" + oDate.getDay();
+        deliveryDateTV.setText(orderDate);
+        String price = CurrentOrder.getDeliveryPrice() + " egp";
+        deliveryPriceTV.setText(price);
+
+        sourceTV.setText(CurrentOrder.getCurrentOrderLocationInfo().getsAdminArea());
+        destinationTV.setText(CurrentOrder.getCurrentOrderLocationInfo().getdAdminArea());
+        switch (CurrentOrder.getVehicle()) {
+            case "Car":
+                vehicleImageIV.setImageResource(R.drawable.vehicle_car_yellow);
+                break;
+            case "Train":
+                vehicleImageIV.setImageResource(R.drawable.vehicle_train_yellow);
+                break;
+            case "MotorCycle":
+                vehicleImageIV.setImageResource(R.drawable.vehicle_motorcycle_yellow);
+                break;
+            case "Metro":
+                vehicleImageIV.setImageResource(R.drawable.vehicle_metro_yellow);
+                break;
+            case "Nos Na2l":
+                vehicleImageIV.setImageResource(R.drawable.vehicle_nos_na2l_yellow);
+                break;
+            case "Bus":
+                vehicleImageIV.setImageResource(R.drawable.vehicle_bus_yellow);
+                break;
+        }
+
+        listItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(context, OrderDetails.class);
+                Log.e("zero one test test", "onClick: "+ CurrentOrder.isPrePaid() );
+                i.putExtra("Package_ID", CurrentOrder);
+                context.startActivity(i);
+            }
+        });
+
+
+        return convertView;
+    }
+}
+
